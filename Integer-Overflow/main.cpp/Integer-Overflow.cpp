1b44dfcb925c9642b8b8e79f3b3bbb242f8ee254
andré <andr3.luiz.viana@gmail.com>
2023-10-29 19:20:45 -0300

    C++ code with exploitable vulnerabilities

---
+++ Integer-Overflow/main.cpp/Integer-Overflow.cpp
@@ -0,0 +1,12 @@
+using namespace std;
+
+#include <iostream>
+
+int main() {
+    int x = 2147483647; // Max value for a signed 32-bit integer
+    int y = 1; // Set the value of y to 1
+    int z = x + 7; // Add x and y together and store the value in z
+    cout << "The value of z is: " << z << endl; // Print out the value of z
+    return 0;
+}
+
---
+++ Null-Pointer-Deference/main.cpp/Null-Pointer-Deference.cpp
@@ -0,0 +1,10 @@
+using namespace std;
+
+#include <iostream>
+
+int main() {
+    int* ptr = NULL; // Integer pointer is initialized to NULL
+    cout << "ptr is pointing to the value" << *ptr << endl; // Integer pointer is dereferenced and printed
+    return 0;
+}
+
